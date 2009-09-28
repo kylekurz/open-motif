e564a6e076b2be0ee94f520af11c64158c0f3ec6
rami.alouran <rami.alouran@b0447be8-a391-11de-9ca8-173d21a9ba08>
2009-09-28 19:55:35 +0000

    Alphabet independce for models

--- trunk/source/Rmes_model.cpp
+++ trunk/source/Rmes_model.cpp
@@ -1,3 +1,26 @@
+
+/*
+**********************************************
+// File:        Rmes_model.cpp
+// Purpose:     Define a class for calculating scoring based on RMES model .
+**********************************************
+
+Copyright (C) 2009  Jens Lichtenberg, Kyle Kurz, Lonnie Welch, Frank Drews, Sophie Schbath, Mark
+Hoebeke, Finn Drablos, Geir Kjetil Sandve, Lee Nau, Xiaoyu Liang, Rami Alouran, Matthew Wiley
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 #include "../include/Rmes_model.h"
 
 #include <iostream>
@@ -64,7 +87,7 @@ rmes_model::rmes_model(owef_args *input_list,data *structure)
                 ratio_file << "Word,Word_Count,Expect,Variance,Ratio"<<endl;
         	//cout<<"list num:"<<list->num_words[i+list->minlength-1]<<endl;
         	clock_t start,end;
-        	double duration;
+        	double duration1,duration2;
         	
         	for(int j=0; j<list->num_words[i+list->minlength-1]; j++){
         		
@@ -76,15 +99,15 @@ rmes_model::rmes_model(owef_args *input_list,data *structure)
                         start=clock();
                         expect = condAsExpect(next_word,order,structure);
                         end=clock();
-                        duration = (double)(end-start)/CLOCKS_PER_SEC;
-                        d1=d1+duration;
+                        duration1 = (double)(end-start)/CLOCKS_PER_SEC;
+                        d1=d1+duration1;
                       
                         
                         start=clock();
                         var=condAsVar(next_word,order,structure);
                         end=clock();
-                        duration = (double)(end-start)/CLOCKS_PER_SEC;
-                        d2=d2+duration;
+                        duration2 = (double)(end-start)/CLOCKS_PER_SEC;
+                        d2=d2+duration2;
                         
                         ratio = compute_ratio(expect, var, structure->get_count(next_word));
                         ratio_file<<next_word<<","<<structure->get_count(next_word)<<","<<expect<<","<<var<<","<<ratio<<endl;
@@ -95,140 +118,138 @@ rmes_model::rmes_model(owef_args *input_list,data *structure)
                 }
                 ratio_file.close();
         }
-        //printf("d1:%f d2:%f\n",d1,d2);
+        printf("condAsExpect(s) took:%f condAsVar(s):%f\n",d1,d2);
 }
 
 
 //* compute condAsExpect
-double rmes_model::condAsExpect(const Word &w, const short m, data *structure)
-  {
+double rmes_model::condAsExpect(const Word &w, const short m, data *structure) {
 
-    long leftSubWord=w.substring(0,m);
+
+    long leftSubWord = w.substring(0, m);
     std::stringstream stream_1;
-    stream_1 << Word(m+1,leftSubWord);
+    stream_1 << Word(m + 1, leftSubWord);
     std::string word_1;
     stream_1 >> word_1;
     double expect = double(structure->get_count(word_1));
 
-    for (int l=1; l<w.length()-m;l++) {//for1
-      long curSubWord=w.substring(l,l+m-1);
-      double quantity=0.0;
-      std::stringstream stream_2 ;
-      std::string word_2;
-      for (int letter=0;letter<Alphabet::alphabet->size();letter++){
-          stream_2.clear();
-          stream_2 << Word(m+1,Alphabet::alphabet->factor()*curSubWord+letter);
-          stream_2 >> word_2;
-          quantity+=double(structure->get_count(word_2));
-      }
+    for (int l = 1; l < w.length() - m; l++) {//for1
+        long curSubWord = w.substring(l, l + m - 1);
+        double quantity = 0.0;
+        std::stringstream stream_2;
+        std::string word_2;
+        for (int letter = 0; letter < Alphabet::alphabet->size(); letter++) {
+            stream_2.clear();
+            stream_2 << Word(m + 1, Alphabet::alphabet->factor() * curSubWord + letter);
+            stream_2 >> word_2;
+            quantity += double(structure->get_count(word_2));
+        }
 
-      std::stringstream stream_3;
-      std::string word_3;
-      if (quantity == 0.0) {
-	expect=0.0;
-	break;
-      } else {
-          long neighbor=curSubWord*Alphabet::alphabet->factor()+w[l+m];
-          stream_3.clear();
-          stream_3 << Word(m+1,neighbor);
-          stream_3 >> word_3;
-	  expect*= double(structure->get_count(word_3))/quantity;
-      }
+        std::stringstream stream_3;
+        std::string word_3;
+        if (quantity == 0.0) {
+            expect = 0.0;
+            break;
+        } else {
+            long neighbor = curSubWord * Alphabet::alphabet->factor() + w[l + m];
+            stream_3.clear();
+            stream_3 << Word(m + 1, neighbor);
+            stream_3 >> word_3;
+            expect *= double(structure->get_count(word_3)) / quantity;
+        }
     }
 
     return expect;
-  }
+}
 
 
 //*compute condAsVar with data*structure instead of Counter
-double rmes_model::condAsVar(const Word &w, const short m, data *structure)
-{
-//	cout << "Word " << w << endl;
-	double var = 0.0;
-	double SeqCount = 0.0;
-	double WordCount = 0.0;
-	
-    	//Counter subWordCounter(m + 1, m + 1);
-    	std::stringstream stream_1;
-    	string word_1;
-    	stream_1.clear();
-    	stream_1 << w;
-    	stream_1 >> word_1;
 
-    	double expect = condAsExpect(w, m, structure);
-    	if (expect != 0.0) 
-    	{
-       		vector<string> known_words;
-       		vector<string> known_subwords;
-       		bool flag = 0;
-       		for(int i=0; i<static_cast<int>(word_1.length()-m); i++)
-       		{
-       			string sub = word_1.substr(i, m+1);
-			
-			if(find(known_words.begin(), known_words.end(), sub) == known_words.end())
-	       		{ 	       		
- 	       			known_words.push_back(sub);
-		       		string sw = sub.substr(0,m);  				
-		       	      			       	     
-		       		if(find(known_subwords.begin(), known_subwords.end(), sw) == known_subwords.end())
-		       		{
-		       			double q1 = 0;
-		       			double q2 = 0;
-		       			double neighbor = 0; //for the neighbor index
+double rmes_model::condAsVar(const Word &w, const short m, data *structure) {
+    //	cout << "Word " << w << endl;
+    double var = 0.0;
+    double SeqCount = 0.0;
+    double WordCount = 0.0;
 
- 		       			known_subwords.push_back(sw);
-					
-			     		//cout << "Subword " << sw << " Count " << structure->get_count(sw) << endl;
-			       		//cout << "\t" << sw << endl;    			
-		
-					vector<string> v;
-					v.push_back("a");
-					v.push_back("c");
-					v.push_back("g");
-					v.push_back("t");
-		
-					for(int j=0; j<4;j++)
-					{
-						string temp = sw;
-						int count = structure->get_count(temp.append(v[j]));				
-						q1 += count;
-						
-						if(flag == 0)
-						{
-//							cout << temp << count << endl;
-							SeqCount += count;
-						}
-						
-						if(count > 0)
-						{									     			
-				     	  		double internal_q2 = 0;
-						
-							for(int p=0; p<static_cast<int>(word_1.length()-m); p++)
-					       		{
-					     	  		string psub = word_1.substr(p, m+1);
-					     	  		//cout << psub << " " << temp << endl;
+    //Counter subWordCounter(m + 1, m + 1);
+    std::stringstream stream_1;
+    string word_1;
+    stream_1.clear();
+    stream_1 << w;
+    stream_1 >> word_1;
 
-								if(psub.compare(temp) == 0)
-								{
-									q2++;
-									internal_q2++;
-								}
-					       		}
-					       		
-  							neighbor += internal_q2 * internal_q2 / count;
-					       	}
-					}
-					if(flag == 0)			
-					{
-						WordCount = q2;
-					}
-					flag =1;
-					var += (q2 * q2) / q1;
-					var -= neighbor;
-				}
-			}           
-            	}
+    double expect = condAsExpect(w, m, structure);
+    if (expect != 0.0) {
+        vector<string> known_words;
+        vector<string> known_subwords;
+        bool flag = 0;
+        for (int i = 0; i<static_cast<int> (word_1.length() - m); i++) {
+            string sub = word_1.substr(i, m + 1);
+
+            if (find(known_words.begin(), known_words.end(), sub) == known_words.end()) {
+                known_words.push_back(sub);
+                string sw = sub.substr(0, m);
+
+                if (find(known_subwords.begin(), known_subwords.end(), sw) == known_subwords.end()) {
+                    double q1 = 0;
+                    double q2 = 0;
+                    double neighbor = 0; //for the neighbor index
+
+                    known_subwords.push_back(sw);
+
+                    //cout << "Subword " << sw << " Count " << structure->get_count(sw) << endl;
+                    //cout << "\t" << sw << endl;
+                    /*
+                    vector<string> v;
+                    v.push_back("a");
+                    v.push_back("c");
+                    v.push_back("g");                  
+                    v.push_back("t");
+                    */
+
+                    vector<string> v;                   
+                    string tmp="";
+                    for (long baseIndex=0;baseIndex<Alphabet::alphabet->size(); baseIndex++){                       
+                        //cout<<"char:"<<Alphabet::alphabet->character(baseIndex)<<" baseIndex:"<<baseIndex<<endl;
+                        tmp=Alphabet::alphabet->character(baseIndex);
+                        v.push_back(tmp);
+                    }
+                    for (int j = 0; j < 4; j++) {
+                        string temp = sw;
+                        int count = structure->get_count(temp.append(v[j]));
+                        q1 += count;
+
+                        if (flag == 0) {
+                            //							cout << temp << count << endl;
+                            SeqCount += count;
+                        }
+
+                        if (count > 0) {
+                            double internal_q2 = 0;
+
+                            for (int p = 0; p<static_cast<int> (word_1.length() - m); p++) {
+                                string psub = word_1.substr(p, m + 1);
+                                //cout << psub << " " << temp << endl;
+
+                                if (psub.compare(temp) == 0) {
+                                    q2++;
+                                    internal_q2++;
+                                }
+                            }
+
+                            neighbor += internal_q2 * internal_q2 / count;
+                        }
+                    }
+                    if (flag == 0) {
+                        WordCount = q2;
+                    }
+                    flag = 1;
+                    var += (q2 * q2) / q1;
+                    var -= neighbor;
+                }
+            }
         }
+    }
 
 //	cout << SeqCount << " " << WordCount << endl;
 
@@ -313,7 +334,7 @@ double rmes_model::condAsVar(const Word &w, const short m, const Counter &cc)
     }
 
     return var;*/
-    return 0;
+    
 
 }
  
@@ -323,8 +344,6 @@ void rmes_model::compute_scores(scores *word, string &motif, data *structure, in
    throw(-1);
    
 }
-
-
 
 //*rmes
 double rmes_model::compute_E(string &motif, data *structure, int &order)
