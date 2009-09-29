8eb12efc127ed2a06f7e38dee1937ca2a38b9d9e
kyle.w.kurz <kyle.w.kurz@b0447be8-a391-11de-9ca8-173d21a9ba08>
2009-09-29 15:02:13 +0000

    made it prettier

--- include/Rmes_model.h
+++ include/Rmes_model.h
@@ -59,32 +59,32 @@ class rmes_model :  public word_scoring
       double compute_ratio(double expect,double variance, int word_count);
       
     
-    //condAsExpect
-    double  condAsExpect(const rmes::Word  &w, const short m, data *structure);
-    //condAsVar with data*
-    double condAsVar(const rmes::Word &w, const short m, data *structure);
-    //condAsVar   using rmes_counter derived class
-    double condAsVar(const rmes::Word &w, const short m, const rmes::Counter &c);   
-    //function to compute all scores for a motif
-      void compute_scores(scores *word, string &motif, data *structure, int &order);
-    //function to compute the E value of a motif
-      double compute_E(string &motif, data *structure, int &order);
-    //function to compute the Es value of a motif
-      double compute_Es(string &motif, data *structure, int &order);
-    //function to compute the Oln value of a motif
-      double compute_Oln(string &motif, data *structure, int &order);
-    //function to compute the Sln value of a motif
-      double compute_Sln(string &motif, data *structure, int &order);
-    //function to compute the pval value of a motif
-      double compute_pval(string &motif, data *structure);
-    //function to compute the rank value of a motif  
-      int compute_rank(string &motif, data *structure); 
-    //function to compute the count value of a motif  
-      int compute_count(string &motif, data *structure);
-    //function to compute the seqs value of a motif  
-      int compute_seqs(string &motif, data *structure);                                                    
-    //function to give clustering access to seeds
-      vector<pair<string, scores> > get_seeds();  
+	//condAsExpect
+	double  condAsExpect(const rmes::Word  &w, const short m, data *structure);
+	//condAsVar with data*
+	double condAsVar(const rmes::Word &w, const short m, data *structure);
+	//condAsVar   using rmes_counter derived class
+	double condAsVar(const rmes::Word &w, const short m, const rmes::Counter &c);   
+	//function to compute all scores for a motif
+	void compute_scores(scores *word, string &motif, data *structure, int &order);
+	//function to compute the E value of a motif
+	double compute_E(string &motif, data *structure, int &order);
+	//function to compute the Es value of a motif
+	double compute_Es(string &motif, data *structure, int &order);
+	//function to compute the Oln value of a motif
+     double compute_Oln(string &motif, data *structure, int &order);
+	//function to compute the Sln value of a motif
+     double compute_Sln(string &motif, data *structure, int &order);
+	//function to compute the pval value of a motif
+     double compute_pval(string &motif, data *structure);
+	//function to compute the rank value of a motif  
+     int compute_rank(string &motif, data *structure); 
+	//function to compute the count value of a motif  
+     int compute_count(string &motif, data *structure);
+	//function to compute the seqs value of a motif  
+	int compute_seqs(string &motif, data *structure);                                                    
+	//function to give clustering access to seeds
+     vector<pair<string, scores> > get_seeds();  
   
   
   protected:
