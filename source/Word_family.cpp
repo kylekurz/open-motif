820b601dbc1fc10c171c417c53ff591a8c175ef8
lichtenj <lichtenj@b0447be8-a391-11de-9ca8-173d21a9ba08>
2009-10-05 11:50:36 +0000

    Integrated support for multiple ambiguous nucleotides in Word Family Analysis

--- source/Word_family.cpp
+++ source/Word_family.cpp
@@ -45,7 +45,11 @@ word_family::word_family(owef_args *input_list,data *structure, word_scoring *mo
 	list=input_list;
 	
 	cout << "Creating Word Families" << endl;	
+  int no_n = 3;
   
+  clock_t start,end;
+  double duration_covar  = 0.0;
+	double duration_create = 0.0;
 	
 	int num_files = (list->maxlength - list->minlength)+1;
 	for(int i=0; i<num_files; i++)
@@ -59,25 +63,91 @@ word_family::word_family(owef_args *input_list,data *structure, word_scoring *mo
   	ratio_file << "Motif,Ratio" << endl;
     
     vector<string> v;
+    vector<string> families;
     
-  	for(int j=0; j<list->num_words[i+list->minlength-1]; j++)
+    int terminator = 0;
+    
+    if(0)
+    {
+    	terminator = 50;
+    }
+    else
+    {
+    	terminator = list->num_words[i+list->minlength-1];
+    }
+    
+    start=clock();
+  	for(int j=0; j < terminator; j++)
 		{
-	    string next_word = structure->get_next_word(i+list->minlength);	    
+	    string next_word = structure->get_next_word(i+list->minlength);
+	    
 	    for (int i=1; i<static_cast<int> (next_word.length()-1); i++) 
 			{
 				char x = next_word[i];
 				next_word[i] = 'N';
-				
-				if(find(v.begin(), v.end(), next_word) == v.end()) 
-				{
-					v.push_back(next_word);
-	    		ratio_file << next_word << "," << create_family(next_word,structure, model, list->order) << endl;
-	    	}	    	
+				if(find(families.begin(), families.end(), next_word) == families.end())
+				{	
+					families.push_back(next_word);
+				}
+	    	
 	    	next_word[i] = x;
 	    }
+	  }
+
+		int temp_n = no_n;;
+		if(no_n - i+list->minlength < 2)
+		{
+  		temp_n = no_n - i+list->minlength;
   	}
+  	
+  	while(temp_n > 1)
+  	{
+	  	for(int u=0; u < static_cast<int>(families.size()); u++)
+			{
+				string word = families[u];
+		   	for (int v=1; v < static_cast<int> (word.length()-1); v++) 
+				{
+					char x = word[v];
+					word[v] = 'N';
+					
+					if(find(families.begin(), families.end(), word) == families.end())
+					{				
+						families.push_back(word);
+		    	}
+		    	word[v] = x;
+		    }
+		  }
+		  temp_n--;
+	  }
+	  end=clock();
+	  
+	  duration_create += (double)(end-start)/CLOCKS_PER_SEC;
+	  
+	  start=clock();
+	  for(int j=0; j < static_cast<int>(families.size()); j++)
+	  {    
+	  	string motif = families[j];
+	  	//cout << "\tScoring(" << motif << ")" << endl;
+			double ratio = create_family(motif,structure, model, list->order);
+			//cout << "Done Scoring" << endl;
+			if(ratio != 0)
+			{	
+	   		ratio_file << motif << "," << ratio << endl;
+	   		//cout << motif << "," << ratio << endl;
+	   	}
+	   	//cout << "Done with " << motif << endl;
+  	}
+  	//cout << "Done with Loop" << endl;
+	  end=clock();
+	  
+	  duration_covar += (double)(end-start)/CLOCKS_PER_SEC;
+	  
   	ratio_file.close();
+  	//cout << "Done Printing" << endl;
   }
+  cout << "Get Next Word took " << duration_create << " seconds" << endl;
+  cout << "CoVariance took    " << duration_covar  << " seconds" << endl;
+  
 }
 
 
@@ -93,35 +163,49 @@ double word_family::create_family(string w,data *structure, word_scoring *model,
 		double count = 0;
 		vector<string> temp = structure->get_regex_matches(w);
 		
-		//cout << "size " << temp.size() << endl;
-		
-		for(int i=0; i < static_cast<int>(temp.size()); i++)
+		if(static_cast<int>(temp.size() > 1))
 		{
-			scores *t = new scores;
-			model->compute_scores(t, temp[i], structure, order);			
-			
-			expect += t->expect;
-			var += t->variance;
-			count += structure->get_count(temp[i]);
-			
-			//Compute the Covariance
-			for(int j=i+1; j < static_cast<int>(temp.size()); j++)
+			for(int i=0; i < static_cast<int>(temp.size()); i++)
 			{
-				scores *s = new scores;
-				model->compute_scores(s, temp[j], structure, order);
-				covar += condAsCoVar(temp[i],temp[j],order,structure,t,s, model);				
-				//cout << "Exploded Word: " << temp[i] << " vs. " <<  temp[j] << endl;
-			}			
+				scores *t = new scores;
+				model->compute_scores(t, temp[i], structure, order);			
+				
+				expect += t->expect;
+				var += t->variance;
+				count += structure->get_count(temp[i]);
+				
+				for(int j=i+1; j < static_cast<int>(temp.size()); j++)
+				{
+					scores *s = new scores;
+					model->compute_scores(s, temp[j], structure, order);
+					double temp_covar = condAsCoVar(temp[i],temp[j],order,structure,t,s, model);
+					//cout << temp[i] << " " << temp[j] << " " << temp_covar << " " << t->expect << " " << s->expect << endl;
+					covar += temp_covar;
+				}				
+			}
+			
+			var += covar;
+			//if(var < 0)
+			//{
+				//cout << "Word " << w << " Var " << var << " Expect " << expect << " Covar " << covar << endl;
+				ratio = (count - expect) / std::sqrt(var); // Needs to be (O - E) / sqrt(V)
+			//}
 		}
-		var += covar;
-		
-		ratio = expect / std::sqrt(var); // Needs to be (O - E) / sqrt(V)
   }
 	return ratio;
 }
 
 double word_family::condAsCoVar(string w1, string w2, int m, data *structure, scores *t, scores *s, word_scoring *model)
 {
+  Word word1 = w1;
+  Word word2 = w2;
+	if(word1.number() > word2.number())
+	{
+		string temp = w1;
+		w1 = w2;
+		w2 = temp;
+	}
+	//cout << "W1: " << w1 << " W2 " << w2 << endl;
   double covar=0.0;
   
   if (w1.compare(w2) == 0) 
@@ -220,25 +304,26 @@ double word_family::condAsCoVar(string w1, string w2, int m, data *structure, sc
       	
       	for (int i = 0; i<static_cast<int> (w1.length() - m); i++) 
     		{ 
-					string sub1 = w1.substr(i, m + 1);
+					string sub1 = w2.substr(i, m + 1);
 					if (sub1.compare(temp1) == 0)
 					{
-						countW1W2++;
+						countW2W1++;
 					}
 					
-					string sub2 = w2.substr(i, m + 1);
+					string sub2 = w1.substr(i, m + 1);
 					if (sub2.compare(temp2) == 0)
 					{
-						countW2W1++;
+						countW1W2++;
 					}
 				}
       	      	
         countW1Seq+=structure->get_count(temp1);
+        //cout << "temp2:" << temp2 << endl;
         countW2Seq+=structure->get_count(temp2);
       }
 
-      covar -= countW2W1 /  countW2Seq;
-      covar -= countW1W2 /countW1Seq ;
+			covar -= countW2W1 / countW1Seq; //Checked and count W1Seq is right
+			covar -= countW1W2 / countW2Seq; //Checked and it is the same as countW2W1 / countW1Seq when the seqs are inverted
 
       if (w1.substr(0,m).compare(w2.substr(0,m)) == 0)
       {
@@ -247,12 +332,9 @@ double word_family::condAsCoVar(string w1, string w2, int m, data *structure, sc
 			
       covar *= expectW1 * expectW2;
 
-      //
-      // Up to here 100% result identity
-      //
       Word word1 = w1;
       Word word2 = w2;
-      
+			
       for (int d = m + 1 - (int)w1.length() ; d < (int)w1.length()-m ; d++) 
       {
 	  		auto_ptr<Word> overlapWord(word1.overlaps(word2,d));
@@ -272,6 +354,7 @@ double word_family::condAsCoVar(string w1, string w2, int m, data *structure, sc
       }
     }
   }
+  //cout << w1 << " vs " << w2 << " Covar " << covar << endl;	
   return covar;
 }
 
