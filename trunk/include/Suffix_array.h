17ac08ec78204f0ee4cac0dc19f568a8de592f8b
lee.nau <lee.nau@b0447be8-a391-11de-9ca8-173d21a9ba08>
2009-10-18 20:57:49 +0000

    Added basic suffix array files.

---
+++ trunk/include/Suffix_array.h
@@ -0,0 +1,32 @@
+#include <stdint.h>
+#include "Data_structure.h"
+
+#ifndef SUFFIX_ARRAY
+#define SUFFIX_ARRAY
+
+class suffix_array : public data
+{
+    public:
+	suffix_array();
+	suffix_array(owef_args *from_input);
+	~suffix_array();
+
+	int get_count(string motif);
+	int get_seqs(string motif);
+	scores* get_stats(string motif);
+	vector<string> get_regex_matches(string regex);
+	vector<string> get_seq_file();
+	
+	int set_stats(string motif, scores *new_stats);
+	
+	string get_next_word(int length);
+	string get_next_extension(string seed, int length);
+	void output();
+	void reset();
+
+    private:
+	uint32_t *suffix_array;
+	uint32_t array_size;
+};
+
+#endif // SUFFIX_ARRAY
