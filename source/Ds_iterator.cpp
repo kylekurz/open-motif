99d976fee4f6e5aa04cf449435e1841b7d463142
kyle.w.kurz <kyle.w.kurz@b0447be8-a391-11de-9ca8-173d21a9ba08>
2009-10-29 19:03:41 +0000

    added iterator base class for getting information out of a data structure

---
+++ source/Ds_iterator.cpp
@@ -0,0 +1,48 @@
+/*
+
+**********************************************
+// File:	Ds_iterator.cpp
+//
+// Author:	Kyle Kurz
+//
+// Date:	10.28.09
+//
+// Purpose:	Define an abstract iterator class for use
+//		with various data structures and
+//		scoring models in the Open Word
+//		Enumeration Framework.
+**********************************************
+
+Copyright (C) 2009  Jens Lichtenberg, Kyle Kurz, Lonnie Welch, Frank Drews, Sophie Schbath, Mark
+Hoebeke, Finn Drablos, Geir Kjetil Sandve, Lee Nau, Xiaoyu Liang, Rami Alouran, Matthew Wiley
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+			    
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+
+
+#include "Ds_iterator.h"
+
+//standard constructor
+ds_iterator::ds_iterator()
+{
+	//no-op
+}
+
+//standard destructor
+ds_iterator::~ds_iterator()
+{
+	list = NULL:
+}
